6cc5ca451cacba60377909253fe3752d997efcf0
aiuhjv1 <ajisrael@mtu.edu>
2022-04-06 13:34:14 -0400

    r   - moved garden bed config request to separate function

--- garden-bed/garden-bed.ino
+++ garden-bed/garden-bed.ino
@@ -7,51 +7,58 @@ unsigned long lastTime = 0;        // Holds value for millis() for timing API ca
 unsigned long apiCallDelay = 5000; // Delay between calls to API
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (apiCallDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
   if ((millis() - lastTime) > apiCallDelay) {
     //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
+    if (WiFi.status() == WL_CONNECTED) {
 
-      String serverPath = serverName + "/"; // + "?temperature=24.37";
+      getGardenBedConfig();
       
-      // Your Domain name with URL path or IP address with path
-      http.begin(serverPath.c_str());
-      
-      // Send HTTP GET request
-      int httpResponseCode = http.GET();
-      
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      // Free resources
-      http.end();
     }
     else {
       Serial.println("WiFi Disconnected");
     }
     lastTime = millis();
+  }
+}
+
+void getGardenBedConfig() {
+  HTTPClient http;
+  
+  String currentPath = serverName + "/api/esp32config/gardenBed";
+
+  http.begin(currentPath.c_str());
+  int respCode = http.GET();
+
+  logHttpResponse("GET", currentPath, respCode, http.getString());
+
+  http.end();
+}
+
+void logHttpResponse(String requestType, String requestPath, int responseCode, String payload) {
+  Serial.print("Request made to: ");
+  Serial.println(requestType + " " + requestPath);
+
+  if (responseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(responseCode);
+    Serial.println(payload);
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(responseCode);
   }
 }

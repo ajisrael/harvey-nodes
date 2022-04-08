00ee92ba072ad5a26353c54d4f836c3883935a8f
aiuhjv1 <ajisrael@mtu.edu>
2022-04-08 09:57:38 -0400

    f   - parsing json from http get request to backend api

--- garden-bed/garden-bed.ino
+++ garden-bed/garden-bed.ino
@@ -1,6 +1,7 @@
 
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "LocalConfig.h"
 
 unsigned long lastTime = 0;        // Holds value for millis() for timing API calls
@@ -42,6 +43,20 @@ void getGardenBedConfig() {
   String currentPath = serverName + "/api/esp32config/gardenBed";
 
   String payload = httpGETRequest(currentPath);
+
+  StaticJsonDocument<300> doc;
+  DeserializationError error = deserializeJson(doc, payload);
+
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  updateApiCallDelay(doc);
+
+}
+
 String httpGETRequest(String requestPath) {
   WiFiClient client;
   HTTPClient http;
@@ -74,5 +89,18 @@ void logHttpResponse(String requestType, String requestPath, int responseCode, S
   else {
     Serial.print("Error code: ");
     Serial.println(responseCode);
+  }
+}
+
+void updateApiCallDelay(StaticJsonDocument<300> doc) {
+  if (!doc["apiCallDelay"]) {
+    Serial.println("No apiCallDelay configuration found");
+    return;
+  }
+
+  if (apiCallDelay != doc["apiCallDelay"]) {
+    apiCallDelay = doc["apiCallDelay"];
+    Serial.print("apiCallDelay updated to: ");
+    Serial.println(apiCallDelay);
   }
 }
